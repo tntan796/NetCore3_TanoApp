010c839cf56b131e6bc690348e03a0a3dc2f31d5
Tan Tano <tantn1@sunshinegroup.vn>
2020-10-29 17:05:10 +0700

    login

---
+++ TanoApp/TanoApp/wwwroot/app/controllers/login/index.js
@@ -0,0 +1,33 @@
+﻿var loginController = function () {
+    this.initialize = function () {
+
+    }
+
+    var registerEvents = function () {
+        $('#btnLogin').on('click', function (e) {
+            e.preventDefault();
+            var user = $('#txtUserName');
+            var pass = $('#txtPassword');
+
+        })
+    }
+
+    var login = function (user, pass) {
+        $.ajax({
+            type: 'POST',
+            data: {
+                userName: user,
+                passWord: pass
+            },
+            dataType: 'json',
+            url: '/admin/login/authen',
+            sucess: function (res) {
+                if (res.sucess) {
+                    window.location.href = "/Admin/Home/Index";
+                } else {
+                    alert("Đăng nhập không đúng");
+                }
+            }
+        })
+    }
+}

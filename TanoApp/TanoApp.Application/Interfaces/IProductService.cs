e580d7e5559b9eb4e072016c8d830fa18ba85876
Tan Tano <tntan796@gmail.com>
2020-12-01 21:09:01 +0700

    add edit delete product category

--- TanoApp/TanoApp.Application/Interfaces/IProductService.cs
+++ TanoApp/TanoApp.Application/Interfaces/IProductService.cs
@@ -12,5 +12,10 @@ namespace TanoApp.Application.Interfaces
     {
         List<ProductViewModel> GetListProduct();
         PagedResult<ProductViewModel> GetAllPaging(int? categoryId, string keyword, int page, int pageSize);
+        ProductViewModel Add(ProductViewModel product);
+        void Update(ProductViewModel product);
+        void Delete(int id);
+        ProductViewModel GetById(int id);
+        void Save();
     }
 }

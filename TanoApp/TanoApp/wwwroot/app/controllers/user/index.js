2b014dc245edbc976cc45e39e19325dedbb28b92
Tan Tano <tntan796@gmail.com>
2020-12-15 22:41:01 +0700

    Add UI User manager

--- TanoApp/TanoApp/wwwroot/app/controllers/user/index.js
+++ TanoApp/TanoApp/wwwroot/app/controllers/user/index.js
@@ -4,6 +4,78 @@
         registerEvents();
     }
 
+    function loadData(isPageChanged) {
+        $.ajax({
+            type: 'GET',
+            url: '/admin/user/getallpaging',
+            data: {
+                categoryId: $("#ddl-category-serach").val(),
+                keyword: $('#txt-search-keyword').val(),
+                page: tano.configs.pageIndex,
+                pageSize: tano.configs.pageSize
+            },
+            dataType: 'json',
+            beforeSend: function () {
+                tano.startLoading();
+            },
+            success: function (response) {
+                var template = $("#table-template").html();
+                var render = "";
+                if (response.rowCount > 0) {
+                    $.each(response.results, function (i, item) {
+                        render += Mustache.render(template, {
+                            Account: item.account,
+                            FullName: item.fullName,
+                            Id: item.id,
+                            UserName: item.userName,
+                            Avatar: item.avatar === null ? '<img src="https://lh3.googleusercontent.com/proxy/xgail7fMYaoDLNjH4HWCq1c21sbtCqZ6J23JHwmdP5DdQUaxF460pEz5y3h-WQqhm6vb_bH2eWli2QtAs-wLrBbvcIVTXqbo0DCpGpb9Uqf6PWc3s2Y" width=25 />' : '<img src="' + item.avatar + '" width=25 />',
+                            DateCreated: tano.dateTimeFormatJson(item.dateCreated),
+                            Status: tano.getStatus(item.status)
+                        });
+                    });
+                    $("#lbl-total-records").text(response.rowCount);
+                    if (render !== undefined) {
+                        $("#tbl-body").html(render);
+                    }
+                    wrapPaging(response.rowCount, function () {
+                        loadData();
+                    }, isPageChanged);
+                }
+                else {
+                    $("tbl-body").html("");
+                }
+                tano.stopLoading();
+            },
+            error: function (status) {
+                console.log(status);
+                tano.stopLoading();
+            }
+        })
+    }
+
+    function wrapPaging(recordCount, callback, changePageSize) {
+        var totalSize = Math.ceil(recordCount / tano.configs.pageSize);
+        // Unbinde pagination if it exist or click change pagesize
+        if ($('#paginationUL a').length === 0 || changePageSize === true) {
+            $('#paginationUL').empty();
+            $('#paginationUL').removeData('twbs-pagination');
+            $('#paginationUL').unbind('page');
+        }
+        //Bind pagination event
+        $('#paginationUL').twbsPagination({
+            totalPages: totalSize,
+            visiblePages: 7,
+            first: '<i class="fa fa-angle-double-left" aria-hidden="true"></i>',
+            prev: '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            next: '<i class="fa fa-angle-right" aria-hidden="true"></i>',
+            last: '<i class="fa fa-angle-double-right" aria-hidden="true"></i>',
+            onPageClick: function (event, p) {
+                tano.configs.pageIndex = p;
+                setTimeout(callback(), 200);
+            }
+        })
+    }
+
     function registerEvents() {
         //Init validation
         $("#frmMaintainance").validate({
@@ -27,14 +99,14 @@
             }
         });
 
-        $("#txt-search-keyword").keypress(function (e) {
+        $("#txtKeyword").keypress(function (e) {
             if (e.which === 13) {
                 e.preventDefault();
                 loadData();
             }
         })
 
-        $("#btn-search").on("click", function () {
+        $("#btnSearch").on("click", function () {
             loadData();
         })
 
@@ -45,10 +117,10 @@
         })
 
 
-        $("#btn-create").on("click", function () {
+        $("#btnCreate").on("click", function () {
             resetFormMaintainance();
             initRoleList();
-            $("#modal-add-edit").modal("show");
+            $("#modalAddEdit").modal("show");
         })
 
         $("body").on("click", ".btn-edit", function (e) {
@@ -197,77 +269,5 @@
                 }
             })
         }
-
-        function loadData(isPageChanged) {
-            $.ajax({
-                type: 'GET',
-                url: '/admin/user/getallpaging',
-                data: {
-                    categoryId: $("#ddl-category-serach").val(),
-                    keyword: $('#txt-search-keyword').val(),
-                    page: tano.configs.pageIndex,
-                    pageSize: tano.configs.pageSize
-                },
-                dataType: 'json',
-                beforeSend: function () {
-                    tano.startLoading();
-                },
-                success: function (response) {
-                    var template = $("#table-template").html();
-                    var render = "";
-                    if (reponse.RowCount > 0) {
-                        $.each(response.Results, function (i, item) {
-                            render += Mustable.render(template, {
-                                FullName: item.FullName,
-                                Id: item.Id,
-                                userName: item.UserName,
-                                Avatar: item.Avatar === undefined ? '<img src = "/admin-side/images/user.png"/>' : '',
-                                DateCreated: tano.dateTimeFormatJson(item.DateCreated),
-                                Status: tano.getStatus(item.Status)
-                            });
-                        });
-                        $("#lbl-total-records").text(response.RowCount);
-                        if (render !== undefined) {
-                            $("#tbl-content").html(render);
-                        }
-                        wrapPing(response.RowCount, function () {
-                            loadData();
-                        }, isPageChanged);
-                    }
-                    else {
-                        $("tbl-content").html("");
-                    }
-                    tano.stopLoading();
-                },
-                error: function (status) {
-                    console.log(status);
-                    tano.stopLoading();
-                }
-            })
-        }
-
-        function wrapPaging(recordCount, callBack, changePageSize) {
-            var totalSize = Math.ceil(recordCount / tano.configs.pageSize);
-        // Unbinde pagination if it exist or click change pagesize
-            if ($('#paginationUL a').length === 0 || changePageSize === true) {
-                $('#paginationUL').empty();
-                $('#paginationUL').removeData('twbs-pagination');
-                $('#paginationUL').unbind('page');
-            }
-            //Bind pagination event
-            $('#paginationUL').twbsPagination({
-                totalPages: totalSize,
-                visiblePages: 7,
-                first: '<i class="fa fa-angle-double-left" aria-hidden="true"></i>',
-                prev: '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-                next: '<i class="fa fa-angle-right" aria-hidden="true"></i>',
-                last: '<i class="fa fa-angle-double-right" aria-hidden="true"></i>',
-                onPageClick: function (event, p) {
-                    tano.configs.pageIndex = p;
-                    setTimeout(callback(), 200);
-                }
-            })
-        }
-
     }
 }
--- TanoApp/TanoApp/wwwroot/app/shared/tano.js
+++ TanoApp/TanoApp/wwwroot/app/shared/tano.js
@@ -40,19 +40,50 @@
 
     },
     dateTimeFormatJson: function (datetime) {
-
+        if (datetime == null || datetime == '')
+            return '';
+        var newdate = new Date(parseInt(datetime.substr(6)));
+        var month = newdate.getMonth() + 1;
+        var day = newdate.getDate();
+        var year = newdate.getFullYear();
+        var hh = newdate.getHours();
+        var mm = newdate.getMinutes();
+        var ss = newdate.getSeconds();
+        if (month < 10)
+            month = "0" + month;
+        if (day < 10)
+            day = "0" + day;
+        if (hh < 10)
+            hh = "0" + hh;
+        if (mm < 10)
+            mm = "0" + mm;
+        if (ss < 10)
+            ss = "0" + ss;
+        return day + "/" + month + "/" + year + " " + hh + ":" + mm + ":" + ss;
     },
     startLoading: function () {
-
+        if ($('.dv-loading').length > 0)
+            $('.dv-loading').removeClass('hide');
     },
     stopLoading: function () {
-
+        if ($('.dv-loading').length > 0)
+            $('.dv-loading')
+                .addClass('hide');
     },
     getStatus: function (status) {
-
+        if (status == 1)
+            return '<label class="label bg-primary">Active</label>';
+        else
+            return '<label class="label bg-danger">InActive</label>';
     },
     formatNumber: function (number, precision) {
+        if (!isFinite(number)) {
+            return number.toString();
+        }
 
+        var a = number.toFixed(precision).split('.');
+        a[0] = a[0].replace(/\d(?=(\d{3})+$)/g, '$&,');
+        return a.join('.');
     },
     unflattern: function (arr) {
         var map = {};

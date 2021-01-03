9a7d9bf536c45cf15a2ec033e97e6a621f2599db
Tan Tano <tntan796@gmail.com>
2021-01-03 09:01:27 +0700

    custom route

---
+++ TanoApp/TanoApp/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TanoApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        [Route("products.html")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
